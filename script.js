8aba9ce2c41e0dd2736d76c45ed178d7a2aa33d5
Richard Robinson Campos <riqui19@gmail.com>
2020-02-21 17:18:10 -0300

    Finalização estilização

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 // menu dropdown mobile
-var mudaImagemMenu = document.getElementById('dropdown');
-var escondeCabecalho = document.getElementById('menu-principal');
+const mudaImagemMenu = document.getElementById('dropdown');
+const escondeCabecalho = document.getElementById('menu-principal');
+const barraBusca = document.getElementById('mobile-search-container');
 
 
 //mostra menu mobile
 function mostraMenuFunction() {
-    var activateButton = document.getElementById('dropdown-itens').classList.toggle('showMenu');
+    const activateButton = document.getElementById('dropdown-itens').classList.toggle('showMenu');
 
+    if (activateButton) {
+        showElements()
+    } else {
+        hideElements()
+    }
+}
+
+function showElements() {
+    mudaImagemMenu.src = 'images/icons/menu-close-icon.png';
+    escondeCabecalho.style = 'display: none';
+    barraBusca.style = 'display: block'
+}
+
+function hideElements() {
+    mudaImagemMenu.src = 'images/icons/menu-icon.png';
+    escondeCabecalho.style = 'display: flex';
+    barraBusca.style = 'display: none'
 }
 
 // fechar menu
 window.onclick = function(event) {
-    if (!event.target.matches('.mobile-menu-icon')) {
-        var dropdown = document.getElementsByClassName("mobile-menu-dropdown-itens");
+    if (!event.target.matches('.mobile-menu-icon') && !event.target.matches('.input-search-stock')) {
+        const dropdown = document.getElementsByClassName("mobile-menu-dropdown-itens");
         for (var i = 0; i < dropdown.length; i++) {
             var openDropdown = dropdown[i];
             if (openDropdown.classList.contains('showMenu')) {
                 openDropdown.classList.remove('showMenu');
+                mudaImagemMenu.src = 'images/icons/menu-icon.png';
+                escondeCabecalho.style = 'display: flex'
+                barraBusca.style = 'display: none'
             }
         }
     }
@@ -25,22 +46,22 @@ window.onclick = function(event) {
 
 //Pegar as informações das poções pelo JSON
 function getJSON() {
-    var xmlHttp = new XMLHttpRequest();
+    const xmlHttp = new XMLHttpRequest();
     xmlHttp.open('GET', "https://cdn.rawgit.com/LucasRuy/1d4a5d45e2ea204d712d0b324af28bab/raw/342e0e9277be486102543c7f50ef5fcf193234b6/potions.json", false);
     xmlHttp.send(null);
-    var myObj = JSON.parse(xmlHttp.responseText);
+    const myObj = JSON.parse(xmlHttp.responseText);
     return myObj;
 }
 
 function showInfos(id) {
-    var myObj = getJSON();
+    const myObj = getJSON();
 
     document.querySelector('#pocao-img').src = 'images/products/' + myObj.potions[id].image;
     document.querySelector('#pocao-nome').innerHTML = myObj.potions[id].name;
     document.querySelector('#pocao-efeito').innerHTML = myObj.potions[id].effect;
     document.querySelector('#pocao-preco').innerHTML = '$' + myObj.potions[id].price;
 
-    var ul = document.querySelector('#pocao-ingrediente');
+    const ul = document.querySelector('#pocao-ingrediente');
     ul.innerHTML = '';
     for (let i = 0; i < myObj.potions[id].ingredients.length; i++) {
         var li = document.createElement('li');
@@ -50,7 +71,7 @@ function showInfos(id) {
 
 }
 
-var pocao = document.getElementsByClassName('produto');
+const pocao = document.getElementsByClassName('produto');
 //Mostrar e fechar Modal
 function iniciaModal(modalID) {
     const modal = document.getElementById(modalID);
